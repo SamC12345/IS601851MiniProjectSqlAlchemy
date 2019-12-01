d41156c1234ff744385df89566570279e07d57ba
Sam <student979@live.com>
2019-12-01 09:11:46 -0500

    added statistics file

---
+++ Statistics/Statistics.py
@@ -0,0 +1,18 @@
+import math
+
+class Statistics:
+    def __init__(self):
+        pass
+
+    def add(self,a,b):
+        return a+b
+    def subtract(self,a,b):
+        return a-b
+    def multiplication(self,a,b):
+        return a*b
+    def division(self,a,b):
+        return a/b
+    def square(self,a):
+        return a*a
+    def sqaureRoot(self,a):
+        return math.sqrt(a)
